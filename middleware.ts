5044648e5ee853c0928295dc1971969bb6fa9e70
czllll <czl010106@163.com>
2024-11-22 20:49:11 +0000

    fix: resolve issue blocking public images from displaying

--- middleware.ts
+++ middleware.ts
@@ -45,6 +45,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        "/((?!api/customAuth|api/auth|api/users|_next|icons|background.png|logo.png|favicon.ico|sitemap.xml).*)"
+        "/((?!api/customAuth|api/auth|api/users|_next|public|icons).*)",
     ]
 };
